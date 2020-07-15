cd96a5311ac0caf58b4efcbec9b2058a8f2bc356
jyarguelles23 <jyarguelles23@gmail.com>
2020-07-14 20:32:51 -0400

    Arrow functions

---
+++ ArrowFunctions/ArrowFunc/js/app.js
@@ -0,0 +1,17 @@
+//Arrow Function
+
+let viajando = (destino) => {
+  return `Viajando a mi destino : ${destino}`;
+};
+
+console.log(viajando("Aruba"));
+
+let viaje = (destino, duracion) => {
+  return `Viajando a mi destino : ${destino}  y la duracion es de : ${duracion}`;
+};
+
+console.log(viaje("Aruba", 30));
+
+let via = (destino) => `Viajando a mi destino : ${destino} `;
+
+console.log(via("Aruba"));
