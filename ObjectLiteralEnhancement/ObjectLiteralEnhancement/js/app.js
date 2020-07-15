6b65fb3d58652a91ec670535ff342e58630d26f1
jyarguelles23 <jyarguelles23@gmail.com>
2020-07-15 01:31:32 -0400

    Object Literal Enhancement

---
+++ ObjectLiteralEnhancement/ObjectLiteralEnhancement/js/app.js
@@ -0,0 +1,18 @@
+//Object Literal  enhancement
+
+//Pone todas las variables en un mismo objeto
+
+const banda = "Metallica";
+const genero = "Heavy Metal";
+const canciones = ["Unforgiven", "Unforgiven 2"];
+
+// old way
+const metallica = {
+  banda: banda,
+  genero: genero,
+  canciones: canciones,
+};
+console.log(metallica);
+// New way
+const metal = { banda, genero, canciones };
+console.log(metal);
