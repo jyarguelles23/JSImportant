4f8469efaf0651b30537ece98530c4cd46d8665c
jyarguelles23 <jyarguelles23@gmail.com>
2020-07-14 21:17:05 -0400

    Objects en JS

---
+++ Objetos/Objetos/js/app.js
@@ -0,0 +1,22 @@
+//Objetos
+//Object Literal old ways
+const persona = {
+  nombre: "Jesus",
+  profesion: "Team Leader",
+  edad: 500,
+};
+
+console.log(persona);
+console.log(persona.nombre);
+console.log(persona.profesion);
+console.log(persona.edad);
+
+// Object Constructor new way
+
+function Tarea(nombre, urgencia) {
+  this.nombre = nombre;
+  this.urgencia = urgencia;
+}
+
+const tarea1 = new Tarea("Yasser", "Urgente");
+const tarea2 = new Tarea("Herson", "Lento");
